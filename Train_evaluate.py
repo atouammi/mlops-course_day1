b3f97325a4e7f15a69abafc99438de4c5fc6ed79
atouammi <akougbanhoun@aimsammi.org>
2024-09-05 01:01:09 +0000

    good merge

--- Train_evaluate.py
+++ Train_evaluate.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
+from data_loader import dataloader
 def train(df):
     X_train, X_test, y_train, y_test = train_test_split(
         df.iloc[:, :-1], df["species"], test_size=0.2, random_state=42
@@ -22,4 +22,4 @@ if __name__ == "__main__":
     iris_df = dataloader()
     model, X_train, X_test, y_train, y_test = train(iris_df)
     accuracy = get_accuracy(model, X_test, y_test)
-    print(f"Accuracy: {accuracy:.2f}")
+    print(f"Accuracy is : {accuracy:.2f}")
